9c8adf465f348b1948f95a887e17aee1c0e70195
Manoj Patra <patra.manoj0@gmail.com>
2022-11-24 11:23:09 +0530

    add procedures file for user

---
+++ src/server/procedures/user.ts
@@ -0,0 +1,103 @@
+import { signJwt } from '~/utils/jwt';
+import { serialize } from 'cookie';
+import { prisma } from '~/server/prisma';
+import { publicProcedure } from '../trpc';
+import {
+  registerUserSchema,
+  requestOtpSchema,
+  verifyOtpSchema,
+} from '../schema/user.schema';
+import { TRPCError } from '@trpc/server';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+export const getUser = publicProcedure.query(async ({ ctx }) => {
+  return ctx.session.user;
+});
+
+export const verifyOtp = publicProcedure
+  .input(verifyOtpSchema)
+  .query(async ({ input, ctx }) => {
+    const decoded = input.hash.split(':');
+    const [id, email] = decoded;
+    const token = await prisma.loginToken.findFirst({
+      where: {
+        id,
+        user: { email },
+      },
+      include: { user: true },
+    });
+
+    if (!token) {
+      throw new TRPCError({
+        code: 'FORBIDDEN',
+        message: 'Invalid token',
+      });
+    }
+
+    const jwt = signJwt({
+      email: token.user.email,
+      id: token.user.id,
+    });
+
+    ctx.res.setHeader('Set-Cookie', serialize('token', jwt, { path: '/' }));
+
+    return { redirect: token.redirect };
+  });
+
+export const requestOtp = publicProcedure
+  .input(requestOtpSchema)
+  .query(async ({ input }) => {
+    const { email, redirect } = input;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'User not found',
+      });
+    }
+
+    await prisma.loginToken.create({
+      data: {
+        redirect,
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    return true;
+  });
+
+export const registerUser = publicProcedure
+  .input(registerUserSchema)
+  .mutation(async ({ input }) => {
+    const { name, email } = input;
+
+    try {
+      const user = await prisma.user.create({
+        data: { email, name },
+      });
+
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new TRPCError({
+            code: 'CONFLICT',
+            message: 'User already exists',
+          });
+        }
+      }
+
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Something went wrong',
+      });
+    }
+  });
--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,102 +1,14 @@
-import { router, publicProcedure } from '../trpc';
-import { TRPCError } from '@trpc/server';
-import { prisma } from '~/server/prisma';
+import { router } from '../trpc';
 import {
-  registerUserSchema,
-  requestOtpSchema,
-  verifyOtpSchema,
-} from '../schema/user.schema';
-import { signJwt } from '~/utils/jwt';
-import { serialize } from 'cookie';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+  verifyOtp,
+  getUser,
+  requestOtp,
+  registerUser,
+} from '../procedures/user';
 
 export const userRouter = router({
-  me: publicProcedure.query(async ({ ctx }) => {
-    return ctx.session.user;
-  }),
-  'verify-otp': publicProcedure
-    .input(verifyOtpSchema)
-    .query(async ({ input, ctx }) => {
-      const decoded = input.hash.split(':');
-      const [id, email] = decoded;
-      const token = await prisma.loginToken.findFirst({
-        where: {
-          id,
-          user: { email },
-        },
-        include: { user: true },
-      });
-
-      if (!token) {
-        throw new TRPCError({
-          code: 'FORBIDDEN',
-          message: 'Invalid token',
-        });
-      }
-
-      const jwt = signJwt({
-        email: token.user.email,
-        id: token.user.id,
-      });
-
-      ctx.res.setHeader('Set-Cookie', serialize('token', jwt, { path: '/' }));
-
-      return { redirect: token.redirect };
-    }),
-  'request-otp': publicProcedure
-    .input(requestOtpSchema)
-    .query(async ({ input }) => {
-      const { email, redirect } = input;
-
-      const user = await prisma.user.findUnique({
-        where: { email },
-      });
-
-      if (!user) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'User not found',
-        });
-      }
-
-      await prisma.loginToken.create({
-        data: {
-          redirect,
-          user: {
-            connect: {
-              id: user.id,
-            },
-          },
-        },
-      });
-
-      return true;
-    }),
-  'register-user': publicProcedure
-    .input(registerUserSchema)
-    .mutation(async ({ input }) => {
-      const { name, email } = input;
-
-      try {
-        const user = await prisma.user.create({
-          data: { email, name },
-        });
-
-        return user;
-      } catch (e) {
-        if (e instanceof PrismaClientKnownRequestError) {
-          if (e.code === 'P2002') {
-            throw new TRPCError({
-              code: 'CONFLICT',
-              message: 'User already exists',
-            });
-          }
-        }
-
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'Something went wrong',
-        });
-      }
-    }),
+  me: getUser,
+  'verify-otp': verifyOtp,
+  'request-otp': requestOtp,
+  'register-user': registerUser,
 });
