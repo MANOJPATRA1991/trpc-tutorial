0b022a81e7c192a10fb50765305a0fba334f6b31
Manoj Patra <patra.manoj0@gmail.com>
2022-11-29 10:38:29 +0530

    add redis client

---
+++ src/server/redis.ts
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+
+const url = `redis://localhost:6379`;
+const redisClient = createClient({ url });
+
+const connectRedis = async () => {
+  try {
+    await redisClient.connect();
+    redisClient.set(
+      'tRPC',
+      'Welcome to tRPC with Next.js, Prisma and TypeScript',
+    );
+    console.log('🚀 Redis client connected...');
+  } catch (e: any) {
+    console.log(e.message);
+    process.exit(1);
+  }
+};
+
+connectRedis();
+
+redisClient.on('error', console.log);
+
+export default redisClient;
