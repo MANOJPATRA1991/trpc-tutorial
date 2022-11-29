55f2fbeb1bdf81bad44f4e3bad453fbf257cedbd
Manoj Patra <patra.manoj0@gmail.com>
2022-11-29 17:06:14 +0530

    update user routes

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -4,11 +4,17 @@ import {
   getUser,
   requestOtp,
   registerUser,
+  loginUser,
+  logoutUser,
+  refreshUserToken,
 } from '../procedures/user';
 
 export const userRouter = router({
   me: getUser,
+  login: loginUser,
   'verify-otp': verifyOtp,
   'request-otp': requestOtp,
   'register-user': registerUser,
+  logout: logoutUser,
+  'refresh-token': refreshUserToken,
 });
