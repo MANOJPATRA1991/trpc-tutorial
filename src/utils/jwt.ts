d24d65b47064d5602e960f2936caa587ac442b31
Manoj Patra <patra.manoj0@gmail.com>
2022-11-29 16:40:53 +0530

    update jwt util functions

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,6 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import { z } from 'zod';
-
-const SECRET = process.env.SECRET || 'changeme';
+import { decode } from './base64';
 
 const JwtData = z.object({
   email: z.string().email(),
@@ -10,10 +9,15 @@ const JwtData = z.object({
 
 type JwtData = z.TypeOf<typeof JwtData>;
 
-export function signJwt(data: JwtData) {
-  return jwt.sign(data, SECRET);
+export function signJwt(key: string, data: JwtData, options: SignOptions = {}) {
+  const privateKey = decode(key);
+  return jwt.sign(data, privateKey, {
+    ...(options && options),
+    algorithm: 'RS256',
+  });
 }
 
-export function verifyJwt<T>(token: string) {
-  return jwt.verify(token, SECRET) as T;
+export function verifyJwt<T>(key: string, token: string) {
+  const publicKey = decode(key);
+  return jwt.verify(token, publicKey) as T;
 }
