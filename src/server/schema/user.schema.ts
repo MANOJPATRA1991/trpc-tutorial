dc2db01f07f263aa9459b510aaa1f1547bb6495a
Manoj Patra <patra.manoj0@gmail.com>
2022-11-29 10:38:46 +0530

    update user schema

--- src/server/schema/user.schema.ts
+++ src/server/schema/user.schema.ts
@@ -1,24 +1,44 @@
-import { z } from 'zod';
+import { TypeOf, z } from 'zod';
 
 export const loginUserSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email('Invalid email or password'),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .email('Invalid email or password'),
 });
 
-export type LoginUserInput = z.infer<typeof loginUserSchema>;
+export type LoginUserInput = TypeOf<typeof loginUserSchema>;
 
-export const registerUserSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-});
+export const registerUserSchema = z
+  .object({
+    name: z.string({ required_error: 'Name is required' }),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email('Invalid email'),
+    photo: z.string({ required_error: 'Photo is required' }),
+    passwordConfirm: z.string({
+      required_error: 'Please confirm your password',
+    }),
+    password: z
+      .string({ required_error: 'Password is required' })
+      .min(8, 'Password must be more than 8 characters')
+      .max(15, 'Password must be less than 32 characters'),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ['passwordConfirm'],
+    message: 'Passwords do not match',
+  });
 
-export type CreateUserInput = z.infer<typeof registerUserSchema>;
+export type CreateUserInput = TypeOf<typeof registerUserSchema>;
 
 export const requestOtpSchema = z.object({
   email: z.string().email(),
   redirect: z.string().default('/'),
 });
 
-export type RequestOtpInput = z.infer<typeof requestOtpSchema>;
+export type RequestOtpInput = TypeOf<typeof requestOtpSchema>;
 
 export const verifyOtpSchema = z.object({
   hash: z.string(),
